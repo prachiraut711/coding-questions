01ccb376647cb4f70b31ca1c7594f8ca3d644088
prachiraut711 <prachiraut711@gmail.com>
2025-06-18 14:05:16 +0530

    new questions  added

--- problem_no_222.py
+++ problem_no_222.py
@@ -1,3 +1,14 @@
+# Delete middle element of a stack
+# Last Updated : 12 Apr, 2025
+# Given a stack with push(), pop(), and empty() operations, The task is to delete the middle element of it without using any additional data structure.
+
+# Input: s = [10, 20, 30, 40, 50]
+# Output: [50, 40, 20, 10]
+# Explanation: The bottom-most element will be 10 and the top-most element will be 50. Middle element will be element at index 3 from bottom, which is 30. Deleting 30, stack will look like [10, 20, 40, 50].
+
+# Input: s = [5, 8, 6, 7, 6, 6, 5, 10, 12, 9]
+# Output: [9, 12, 10, 5, 6, 7, 6, 8, 5]
+
 def midElement(list):
     stack = []
 
--- tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-    # head.next.next.next = Node(2)
-    # head.next.next.next.next = Node(1)
